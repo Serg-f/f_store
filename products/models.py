67933c7cdc62887059d4ee0ddf5b6effcfcbf3af
serg <feelwork2077@gmail.com>
2023-05-08 21:45:03 +0200

    created tables ProdCategory, Products; makemigrations, migrate

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProdCategory(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    image = models.ImageField(upload_to='prod_images/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to=ProdCategory, on_delete=models.CASCADE)
