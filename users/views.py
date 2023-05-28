08c071bd5adb1baf6adabccc033a1fbc82d7db1b
serg <feelwork2077@gmail.com>
2023-05-28 22:13:55 +0200

    user authentication

--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = auth.authenticate(**form.cleaned_data)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print('errors:')
+            print(form.non_field_errors())
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
