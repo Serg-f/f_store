258f4b919f160fae5d0f106e045fcdddc632766b
serg <feelwork2077@gmail.com>
2023-08-15 18:14:55 +0200

    postgreSQL connected

--- f_store/settings.py
+++ f_store/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'f_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "f_store_db",
+        "USER": "adminuser",
+        "PASSWORD": "Adminuser1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
