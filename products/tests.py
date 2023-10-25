cd5575b9bfb1d11b52f70d8d5f89da2b99ade768
serg <feelwork2077@gmail.com>
2023-10-25 14:18:09 +0200

    fix tests

--- products/tests.py
+++ products/tests.py
@@ -13,7 +13,7 @@ class IndexViewTestCase(TestCase):
         response = self.client.get(path)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, 'products/index.html')
-        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertEqual(response.context_data['title'], 'F-Store')
 
 
 class ProductViewTestCase(TestCase):
@@ -28,11 +28,11 @@ class ProductViewTestCase(TestCase):
 
     def test_all_cats(self):
         response = self.client.get(reverse('products:category', args=(0,)))
-        qs_db = Product.objects.all()[:3]
-        self.check_params(response=response, title='Store - Catalog', qs_db=qs_db)
+        qs_db = Product.objects.all()[:6]
+        self.check_params(response=response, title='F-Store - Catalog', qs_db=qs_db)
 
     def test_single_cat(self):
         for cat in ProdCategory.objects.all():
             response = self.client.get(reverse('products:category', args=(cat.id,)))
-            qs_db = cat.product_set.all()[:3]
+            qs_db = cat.product_set.all()[:6]
             self.check_params(response=response, title=cat.name, qs_db=qs_db)
